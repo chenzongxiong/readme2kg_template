a89baafd092abc3346374c93b197b31ab7846569
Zongxiong, Chen <czxczf@gmail.com>
2025-01-26 11:29:25 +0100

    update

--- src/predictor.py
+++ src/predictor.py
@@ -81,7 +81,8 @@ class DummpyPredictor(BasePredictor):
 
 
 if __name__ == "__main__":
-    file_path = './data/231sm_Low_Resource_KBP_master_README.md.tsv'
+    file_path = './data/train/231sm_Low_Resource_KBP_master_README.md.tsv'
+    file_path = './data/test/231sm_Low_Resource_KBP_master_README.md.tsv'
     ref_doc = webanno_tsv_read_file(file_path)
 
     predictor = DummpyPredictor()
