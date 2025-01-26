575cc785a1f670b68d308d70d7b457b9e3f2a457
Zongxiong, Chen <czxczf@gmail.com>
2025-01-26 17:13:05 +0100

    update

--- src/predictor.py
+++ src/predictor.py
@@ -84,27 +84,29 @@ class DummpyPredictor(BasePredictor):
 
 
 if __name__ == "__main__":
-    # Path to annotated files
-    file_path = './data/train/231sm_Low_Resource_KBP_master_README.md.tsv'
-    # Path to un-annotated files
-    file_path = './data/test/231sm_Low_Resource_KBP_master_README.md.tsv'
-    ref_doc = webanno_tsv_read_file(file_path)
+    import os
+    base_path = './data/test'
+    file_names = [fp for fp in os.listdir(base_path) if os.path.isfile(os.path.join(base_path, fp)) and fp.endswith('.tsv')]
+    output_folder = './results/dummy'
+    os.makedirs(output_folder, exist_ok=True)
 
     predictor = DummpyPredictor()
-    predicted_doc = predictor(ref_doc)
+    for file_name in file_names:
+        file_path = os.path.join(base_path, file_name)
+        ref_doc = webanno_tsv_read_file(file_path)
+        predicted_doc = predictor(ref_doc)
 
-    # Verify
-    assert ref_doc.text == predicted_doc.text, 'content changed'
-    assert len(ref_doc.sentences) == len(predicted_doc.sentences), 'sentences changed'
-    assert len(ref_doc.tokens) == len(predicted_doc.tokens), 'tokens changed'
-    for s1, s2 in zip(ref_doc.sentences, predicted_doc.sentences):
-        assert s1 == s2, f'sentence changed, \n{s1}\n{s2}'
+        # Verify
+        assert ref_doc.text == predicted_doc.text, 'content changed'
+        assert len(ref_doc.sentences) == len(predicted_doc.sentences), 'sentences changed'
+        assert len(ref_doc.tokens) == len(predicted_doc.tokens), 'tokens changed'
+        for s1, s2 in zip(ref_doc.sentences, predicted_doc.sentences):
+            assert s1 == s2, f'sentence changed, \n{s1}\n{s2}'
 
-    for t1, t2 in zip(ref_doc.tokens, predicted_doc.tokens):
-        assert t1 == t2, f'token changed: \n{t1}\n{t2}'
+        for t1, t2 in zip(ref_doc.tokens, predicted_doc.tokens):
+            assert t1 == t2, f'token changed: \n{t1}\n{t2}'
 
-    print(f"Predicted {len(predicted_doc.annotations)} annotations")
-    print(predicted_doc.annotations)
-    prediction_path = 'results/prediction/231sm_Low_Resource_KBP_master_README.md.tsv'
-    with open(prediction_path, 'w') as fd:
-        fd.write(predicted_doc.tsv())
+        print(f"Predicted {len(predicted_doc.annotations)} annotations")
+        prediction_path = os.path.join(output_folder, file_name)
+        with open(prediction_path, 'w') as fd:
+            fd.write(predicted_doc.tsv())
