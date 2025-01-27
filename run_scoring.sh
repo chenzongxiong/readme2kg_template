1d32c3459d8609472d8c7047ec3259e7d8f5d866
anntanp <anntanp@gmail.com>
2025-01-27 11:07:39 +0100

    run all 4 tests only when -debug option is true.

--- run_scoring.sh
+++ run_scoring.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+mode="${1:-test}";
+
 python src/scoring.py --reference_dir ./data/train --prediction_dir ./results/prediction
-python src/scoring.py --reference_dir ./data/train --prediction_dir ./results/missing_files
-python src/scoring.py --reference_dir ./data/train --prediction_dir ./results/dummy
-python src/scoring.py --reference_dir ./data/train --prediction_dir ./results/empty
+
+if [ "$mode" = "-debug" ];
+then
+    python src/scoring.py --reference_dir ./data/train --prediction_dir ./results/missing_files
+    python src/scoring.py --reference_dir ./data/train --prediction_dir ./results/dummy
+    python src/scoring.py --reference_dir ./data/train --prediction_dir ./results/empty
+fi;
